9a593530ebf8898f0f1158d63c432a14eabd9e50
Diogo Simões <dsimoes2000@gmail.com>
2022-05-16 22:02:20 +0100

    Adds Project Structure

---
+++ src/main/java/com/cp/correioprivado/CorreioPrivadoApplication.java
@@ -0,0 +1,13 @@
+package com.cp.correioprivado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreioPrivadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorreioPrivadoApplication.class, args);
+    }
+
+}
