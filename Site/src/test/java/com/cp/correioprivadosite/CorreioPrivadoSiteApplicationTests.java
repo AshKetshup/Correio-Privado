549e588a9057dcbfabe39d4f5e5522afc40681d7
Diogo Simões <dsimoes2000@gmail.com>
2022-05-23 23:30:05 +0100

    Adds Project Structure to Frontend & Backend

---
+++ Site/src/test/java/com/cp/correioprivadosite/CorreioPrivadoSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp.correioprivadosite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorreioPrivadoSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
