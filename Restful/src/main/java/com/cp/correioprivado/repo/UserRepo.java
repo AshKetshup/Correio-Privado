3686ae95eac41429671cfee0361c7af8682d04ab
Luís MIguel <farsenfsd@gmail.com>
2022-06-04 18:56:03 +0100

    Changing username to email

--- Restful/src/main/java/com/cp/correioprivado/repo/UserRepo.java
+++ Restful/src/main/java/com/cp/correioprivado/repo/UserRepo.java
@@ -4,6 +4,6 @@ import com.cp.correioprivado.dados.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User,String> {
-    User findBySurname(String surname);
+    User findByEmail(String email);
     User findById(Long id);
 }
