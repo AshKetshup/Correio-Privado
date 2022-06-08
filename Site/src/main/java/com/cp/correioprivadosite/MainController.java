a05945bf099c9788e22178275a0d8db4bbb51b68
CallmeJoca <jorgepissarra@protonmail.com>
2022-06-08 01:26:51 +0100

    Implementation of a cookie handler

--- Site/src/main/java/com/cp/correioprivadosite/MainController.java
+++ Site/src/main/java/com/cp/correioprivadosite/MainController.java
@@ -8,43 +8,78 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.*;
 
 @Controller @Slf4j
 public class MainController {
     private final String restful = "http://vmi827195.contaboserver.net:8080";
+    private final String accessToken = "access_token";
     //Controller for the main page
     @GetMapping(path="/")
-    public String index(Model model, @CookieValue("accesstoken") Cookie userAccessToken){
+    public String index(Model model, HttpServletRequest webRequest, HttpServletResponse webResponse){
         //no honest idea what this needs from the server
+        log.info("User is in index");
+        Cookie[] userCookies = webRequest.getCookies();
 
-        return "/index.html";
+        if(userCookies == null){
+            log.info("No Cookies were found");
+        }else {
+            log.info("Found {} Cookies", userCookies.length);
+            for(int i = 0; i < userCookies.length ; i++){
+                if (userCookies[i].getName() == accessToken){
+                    log.info("Access token found: {}", userCookies[i].getValue());
+                    String accessTokenValue = userCookies[i].getValue();
+                    break;
+                }
+            }
+
+        }
+
+        
+
+        return "/index";
     }
     //Controller for the login page that sends the login info to the restAPI and then gets back the access token
     @GetMapping(path="/login")
     public String login(Model model,
-                        @CookieValue("accesstoken") Cookie userAccessToken) {
-        //Does this work?
-        if(userAccessToken == null){
+                        HttpServletRequest webRequest,
+                        HttpServletResponse webResponse) {
+        Cookie[] userCookies = webRequest.getCookies();
+        log.info("User is in login");
+        if(userCookies == null){
+            log.info("No Cookies were found");
+            return "/login";
+        }else {
+            log.info("Found {} Cookies", userCookies.length);
+            for(int i = 0; i < userCookies.length ; i++){
+                if (userCookies[i].getName() == accessToken){
+                    log.info("Access token found: {}", userCookies[i].getValue());
+                    String accessTokenValue = userCookies[i].getValue();
+                    return "redirect:/";
+                }
+                log.info("Cookie {}: {} -> {}", i, userCookies[i].getName(), userCookies[i].getValue());
+            }
+            log.info("No access token was found");
             model.addAttribute("loginInfo", new LoginForm());
-            return "redirect:/login";
-        }else{
-            return "redirect:/";
         }
+        return "/login";
     }
     @GetMapping(path="/tryLogin")
     public String tryLogin(
         Model model,
         @RequestParam(required = false) String username,
-        @RequestParam(required = false) String password) throws IOException, NullPointerException
+        @RequestParam(required = false) String password,
+        HttpServletRequest webRequest,
+        HttpServletResponse webResponse) throws IOException, NullPointerException
     {
         final String loginURI = restful + "/login";
         log.info("username is {} and password is {}", username, password);
@@ -66,8 +101,9 @@ public class MainController {
             String jsonToken = response.body().string();
             JSONObject token = new JSONObject(jsonToken);
             //Store the token in a cookie
-            Cookie userTokenCookie = new Cookie("accesstoken", token.getString("access_token"));
+            Cookie userTokenCookie = new Cookie(accessToken, token.getString("access_token"));
             userTokenCookie.setMaxAge(10 * 60); //it's in seconds
+            webResponse.addCookie(userTokenCookie);
             return "redirect:/";
         } else {
             return "redirect:/login";
@@ -78,9 +114,8 @@ public class MainController {
             Model model,
             @RequestParam(required = false) Long topicID,
             @RequestParam(required = false) @DateTimeFormat(pattern="yyyy-MM-dd") Date startDate,
-            @RequestParam(required = false) @DateTimeFormat(pattern="yyyy-MM-dd") Date endDate,
-            @CookieValue("accesstoken") Cookie userAccessToken) {
-        log.info("News Controller triggered");
+            @RequestParam(required = false) @DateTimeFormat(pattern="yyyy-MM-dd") Date endDate) {
+        log.info("User is in news");
         RestTemplate restTemplate = new RestTemplate();
 
         List<Topic> listTopics = new ArrayList<>();
@@ -122,10 +157,9 @@ public class MainController {
     }
     //Controller that generates a news article and sends it to the restAPI for storage
     @GetMapping(path="/news_create")
-    public String news_create(Model model,
-                              @CookieValue("accesstoken") Cookie userAccessToken){
+    public String news_create(Model model){
         //send POST to server to add news to /api/
-
+        log.info("User is in news_create");
 
         model.addAttribute("newArticle", new News());
 
@@ -134,18 +168,17 @@ public class MainController {
 
     //Controller that displays the chosen news from /news.html to the user
     @GetMapping(path="/news_viewer")
-    public String news_viewer(Model model, @CookieValue("accesstoken") Cookie userAccessToken) {
+    public String news_viewer(Model model) {
         //query GET to server for news
-
+        log.info("User is in news_viewer");
 
         return "/news_viewer.html";
     }
 
     //Controller that gets the user info from the RestAPI and displays it to the user
     @GetMapping(path="/profile")
-    public String profile(Model model,
-                          @CookieValue("accesstoken") Cookie userAccessToken) throws JSONException{
-
+    public String profile(Model model) throws JSONException{
+        log.info("User is in profile");
         //query POST to server to get personal user info back
         final String profileURI = restful + "/users";
 
@@ -165,11 +198,10 @@ public class MainController {
 
     //Controller that sends a new user to be added to the User Database
     @GetMapping(path="/register.html")
-    public String register(Model model, @CookieValue("accesstoken") Cookie userAccessToken) {
-
+    public String register(Model model) {
+        log.info("User is in register");
         //Does this work?
-        if(userAccessToken != null)
-            return "redirect:/";
+
 
         model.addAttribute("registerInfo", new RegisterForm());
         return "redirect:/register";
@@ -251,9 +283,8 @@ public class MainController {
 
     //Controller that displays all topics from the RestAPI
     @GetMapping(path="/topics")
-    public String topics(Model model,
-                         @CookieValue("accesstoken") Cookie userAccessToken){
-
+    public String topics(Model model){
+        log.info("User is in topics");
         RestTemplate restTemplate = new RestTemplate();
         //query a GET to the server
         final String topicsURI = restful + "/api/topics";
